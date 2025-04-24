fd424f40e9617003df6f71704c3d7281f05f2316
Claes2840 <Claes.r.nielsen@gmail.com>
2025-04-24 22:06:39 +0200

    ARL compiler added

---
+++ Compiler/Read_file.py
@@ -0,0 +1,17 @@
+
+def read_file(input_filename):
+    file = open(input_filename, 'r')
+    lines = file.readlines()
+    lines = [line.strip() for line in lines]
+    return lines
+
+def extract_elms_1_tape(rule):
+    rule = rule.replace('(','')
+    rule = rule.replace(')','')
+    rule = rule.split(',')
+    return rule
+
+def write_file_newline(output_filename, data):
+    outfile = open(output_filename, 'w+')
+    for line in data:
+        outfile.write(line + "\n")
